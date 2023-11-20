d1d7a3c22a8223052947dd3bc492630d5f7949b6
Ujwal Agrawal <uvagrawal123@gmail.com>
2023-11-19 20:32:28 -0700

    largest common substring

---
+++ longest common substring.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    x = input("Enter first string: ")
+    y = input("Enter second string: ")
+
+    m = len(x)
+    n = len(y)
+
+    dp = [[None] * (n+1) for _ in range(m+1)]
+
+    for i in range(m+1):
+        dp[i][0] = 0
+    for j in range(n+1):
+        dp[0][j] = 0
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if (x[i-1] == y[j-1]):
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = 0
+    m = -1
+    for i in dp:
+        m = max(max(i), m)
+    print(f"The largest common substring has length {m}")
