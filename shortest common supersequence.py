e4fb180aa386571e686e6b1ad56bbb8e3a917066
Ujwal Agrawal <uvagrawal123@gmail.com>
2023-11-23 16:44:37 -0700

    Shortest common supersequence

---
+++ shortest common supersequence.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+    x = input("Enter first string: ")
+    y = input("Enter second string: ")
+
+    m = len(x)
+    n = len(y)
+
+    dp = [[None] * (n+1) for _ in range(m+1)]
+
+    for i in range(m+1):
+        dp[i][0] = 0
+    for j in range(n+1):
+        dp[0][j] = 0
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if (x[i-1] == y[j-1]):
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+    print(f"Length of Shortest common supersequence is {n + m - dp[m][n]}")
