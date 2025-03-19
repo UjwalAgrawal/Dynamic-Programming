515ea7c1b54e08ba04de88cb2164abd46b981656
Ujwal Agrawal <uvagrawal123@gmail.com>
2025-03-19 09:05:49 -0700

    optimized the code a bit

--- Knapsack/knapsack_01.py
+++ Knapsack/knapsack_01.py
@@ -1,27 +1,24 @@
 dp = {}
 
-def knapsack(weights, values, totalCapacity, n):
-    if(n==0 or totalCapacity==0):
-        return(0)
-    if((n, totalCapacity) in dp):
+
+def knapsack(totalCapacity, n):
+    if n == 0 or totalCapacity == 0:
+        return 0
+    if (n, totalCapacity) in dp:
         return dp[(n, totalCapacity)]
-    if(weights[n-1] <= totalCapacity):
+    if weights[n - 1] <= totalCapacity:
         dp[(n, totalCapacity)] = max(
-            values[n-1] + knapsack(weights, values, totalCapacity - weights[n-1], n-1), knapsack(weights, values, totalCapacity, n-1)
+            values[n - 1]
+            + knapsack(totalCapacity - weights[n - 1], n - 1),
+            knapsack(totalCapacity, n - 1),
         )
     else:
-        dp[(n, totalCapacity)] = knapsack(weights, values, totalCapacity, n-1)
+        dp[(n, totalCapacity)] = knapsack(totalCapacity, n - 1)
     return dp[(n, totalCapacity)]
-
-
 
 
 weights = list(map(int, input("Enter weights: ").split()))
 values = list(map(int, input("Enter values: ").split()))
 totalCapacity = int(input("Enter knapsack capacity: "))
-maxValue = knapsack(weights, values, totalCapacity, len(weights))
+maxValue = knapsack(totalCapacity, len(weights))
 print(maxValue)
-
-
-
-
