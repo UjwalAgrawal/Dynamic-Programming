9c8c0964866b45771af4d7b0e618ef1cf2cd1d26
Ujwal Agrawal <uvagrawal123@gmail.com>
2025-08-24 11:58:59 -0700

    updated code readability

--- MatrixChainMultiplication/MCM Memoized.py
+++ MatrixChainMultiplication/MCM Memoized.py
@@ -1,26 +1,24 @@
-def solve(arr, i, j):
-    if (i >= j):
-        return (0)
+def solve(i, j):
+    if i >= j:
+        return 0
 
-    m = float('inf')
-    if(dp[i][j] != -1):
-        return dp[i][j]
+    m = float("inf")
+    if (i, j) in dp:
+        return dp[(i, j)]
 
     for k in range(i, j):
-        tempAns = solve(arr, i, k) + solve(arr, k+1, j) + \
-            arr[i-1]*arr[k]*arr[j]
+        tempAns = solve(i, k) + solve(k + 1, j) + arr[i - 1] * arr[k] * arr[j]
         m = min(m, tempAns)
 
-    dp[i][j] = m
-    return (m)
+    dp[(i, j)] = m
+    return m
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     arr = [40, 20, 30, 10, 30]
     n = len(arr)
 
-    global dp
-    dp = [[-1] * (n+1) for _ in range(n+1)]
+    dp = {}
 
-    res = solve(arr, 1, n-1)
-    print(f'The minimum cost of matrix chain multiplication is {res}')
+    res = solve(1, n - 1)
+    print(f"The minimum cost of matrix chain multiplication is {res}")
--- MatrixChainMultiplication/palindrome partitioning.py
+++ MatrixChainMultiplication/palindrome partitioning.py
@@ -1,33 +1,38 @@
-def solve(arr, i, j):
-    if (dp[i][j] != -1):
-        return dp[i][j]
-    if (i >= j):
-        return (0)
-    if arr[i:j+1] == arr[i:j+1][::-1]:
-        return (0)
-    m = float('inf')
+def solve(i, j):
+    if (i, j) in dp:
+        return dp[(i, j)]
+
+    if i >= j:
+        return 0
+
+    if arr[i : j + 1] == arr[i : j + 1][::-1]:
+        return 0
+
+    mini = float("inf")
+
     for k in range(i, j):
-        if(dp[i][k] != -1):
-            left = dp[i][k]
+        if (i, k) in dp:
+            left = dp[(i, k)]
         else:
-            left = solve(arr, i , k)
-        if(dp[k+1][j] != -1):
-            right = dp[k+1][j]
+            left = solve(i, k)
+        if (k + 1, j) in dp:
+            right = dp[(k + 1, j)]
         else:
-            right = solve(arr, k+1 , j)
+            right = solve(k + 1, j)
         tempAns = left + 1 + right
-        m = min(tempAns, m)
-    dp[i][j] = m
-    return (m)
+        mini = min(tempAns, mini)
+
+    dp[(i, j)] = mini
+    return mini
 
 
-if __name__ == '__main__':
-    arr = 'malayalan'
+if __name__ == "__main__":
+    arr = "malayalan"
     n = len(arr)
+    dp = {}
 
-    global dp
-    dp = [[-1] * (n+1) for _ in range(n+1)]
+    res = solve(0, n - 1)
 
-    res = solve(arr, 0, n-1)
-
-    print(f'The minimum number of partitions required to make all the partitions palindrome is {res}')
+    print(
+        f"The minimum number of partitions required to make all the partitions palindrome is {res}"
+    )
